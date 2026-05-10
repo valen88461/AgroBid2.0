ccdea94f4017f24c907d99e13017440c2dc7e7dd
valen88461 <3015609739v@gmail.com>
2026-05-10 08:40:56 -0500

    Delete Tierra-subastas directory

--- Tierra-subastas/guardar_notificacion.php
+++
@@ -1,7 +0,0 @@
-<?php
-function guardarNotificacion($conexion, $usuario_id, $subasta_id, $mensaje, $tipo = 'puja'){
-    $sql = "INSERT INTO notificaciones (usuario_id, subasta_id, mensaje, tipo) 
-            VALUES ($usuario_id, $subasta_id, '$mensaje', '$tipo')";
-    return mysqli_query($conexion, $sql);
-}
-?>
